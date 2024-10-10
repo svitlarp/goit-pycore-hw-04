3f333022f10b8df5042d969e8bd6988e0564223b
svitlarp <svitlanka.ripa@gmail.com>
2024-10-11 01:09:58 +0200

    main
    finish task2, test cases wip

--- task-1.py
+++ task-1.py
@@ -50,36 +50,36 @@ print('file_3 result:', total_salary(file_three))
 print('file_4 result:', total_salary(file_four))
 
 # TestCase1 file with correct data (should find and return total_salary and average_salary)
-assert total_salary(file_one) == (33000, 11000), "Test case 1 failed"
+assert total_salary(file_one) == (6000, 2000), "Test case 1 failed"
 
 # TestCase2 file wich does not exist (should )
-assert total_salary(file_one) == "File does not exist", "Test case 2 failed"
+assert total_salary(file_two) == "File does not exist", "Test case 2 failed"
 
-# TestCase3 file with data in wring format: salary is written out in words.(should return )
-# Question: How to write a test with dynamic data??: result = line + "-> To calculate total salary and average salary salary should be written in numbers"
-assert total_salary(file_one) == "To calculate total salary and average salary salary should be written in numbers", "Test case 3 failed"
+# # TestCase3 file with data in wring format: salary is written out in words.(should return )
+# # Question: How to write a test with dynamic data??: result = line + "-> To calculate total salary and average salary salary should be written in numbers"
+# assert total_salary(file_three) == "To calculate total salary and average salary salary should be written in numbers", "Test case 3 failed"
 
-# TestCase4 file with only one line of text
-assert total_salary(file_four) == file.content,  "Test case 4 failed"
+# # TestCase4 file with only one line of text
+# assert total_salary(file_four) == file.content,  "Test case 4 failed"
 
 
 
-# Create files with data
-employee_data = ("Alex Korp,3000 \n"
-        "Nikita Borisenko,2000 \n"
-        "Sitarama Raju,1000")
+# # Create files with data
+# employee_data = ("Alex Korp,3000 \n"
+#         "Nikita Borisenko,2000 \n"
+#         "Sitarama Raju,1000")
 
-def create_files(num, data):
-    if isinstance(num, int) and num:
-        filename = 'file_0'
-        for i in range(1, 6):
-            with open(filename, "w") as file:
-                file.writelines(data)
-            filename = filename[:-1] + str(i)
-    else:
-        print('Parameter `num` should be an integer to be able to create range of files')        
+# def create_files(num, data):
+#     if isinstance(num, int) and num:
+#         filename = 'file_0'
+#         for i in range(1, 6):
+#             with open(filename, "w") as file:
+#                 file.writelines(data)
+#             filename = filename[:-1] + str(i)
+#     else:
+#         print('Parameter `num` should be an integer to be able to create range of files')        
 
-create_files(4, employee_data)
+# create_files(4, employee_data)
 
 
 
---
+++ task-2.py
@@ -0,0 +1,76 @@
+
+
+def get_cats_info(file_path):
+    """ 
+    This function read a file and create a list of dict  where every dict contain info about one cat.
+    The get_cats_info() function is designed to process a text file containing information about cats.
+    Each record of the file corresponds to a specific cat and includes id, name and age of a cat separated by a comma.
+
+    Parameters:
+    path (str): Path to the file 
+
+    Returns:
+    (list of dictionaries) The list of dict where each dict represents one cat.
+    """
+    cats_list = []
+    cats_keys = ['id' , 'name' , 'age']
+    try:
+        with open(file_path, "r") as file_obj:
+            # TODO Check if file is empty
+
+            for line in file_obj.readlines():
+
+                # With handly split() method extract all values separatly by specifying a comma as a separator
+                line_data = line.strip('\n').split(',')   
+
+                # Line by line append a new dictionary to the list        
+                cats_list.append({k:v for (k, v) in zip(cats_keys, line_data)})
+            return cats_list
+    except FileNotFoundError:
+        return "File does not exist"            
+
+
+# print(get_cats_info('/home/unixsv/python_module_goit/goit-pycore-hw-04/cats.txt'))
+print(get_cats_info('cats.txt'))
+
+
+file_one = 'cats.txt'
+file_two = 'rich.ftp'
+file_three = 'cats3.txt'
+file_four = 'cats4.txt'
+
+# TestCase1 file with correct data (should find and return total_salary and average_salary)
+assert get_cats_info(file_one) == [{'id': '60b90c1c13067a15887e1ae1', 'name': 'Tayson', 'age': '3'}, {'id': '60b90c2413067a15887e1ae2', 'name': 'Vika', 'age': '1'}, {'id': '60b90c2e13067a15887e1ae3', 'name': 'Barsik', 'age': '2'}, {'id': '60b90c3b13067a15887e1ae4', 'name': 'Simon', 'age': '12'}, {'id': '60b90c4613067a15887e1ae5', 'name': 'Tessi', 'age': '5'}], "Test case 1 failed"
+
+# TestCase2 file wich does not exist (should )
+assert get_cats_info(file_two) == "File does not exist", "Test case 2 failed"
+
+# # TestCase3 Empty file (should )
+# assert get_cats_info(file_three) == "File does not exist", "Test case 2 failed"
+
+# # TestCase4 data problem (should )
+# assert get_cats_info(file_four) == "File does not exist", "Test case 2 failed"
+
+
+
+
+
+# d = {key:value for key in line_data for value in x} 
+# d = {key:value for key in x for value in line_data} # Question: why works unexpected in dict comprehension?
+# cats_list.append({k:v for (k, v) in zip(cats_keys, line_data)})
+
+
+
+cats_data = """60b90c1c13067a15887e1ae1,Tayson,3
+60b90c2413067a15887e1ae2,Vika,1
+60b90c2e13067a15887e1ae3,Barsik,2
+60b90c3b13067a15887e1ae4,Simon,12
+60b90c4613067a15887e1ae5,Tessi,560b90c1c13067a15887e1ae1,Tayson,3
+60b90c2413067a15887e1ae2,Vika,1
+60b90c2e13067a15887e1ae3,Barsik,2
+60b90c3b13067a15887e1ae4,Simon,12
+60b90c4613067a15887e1ae5,Tessi,5"""
+
+
+# with open("cats.txt", 'w') as file:
+#     file.write(cats_data)
