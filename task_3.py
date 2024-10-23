7ed25e1c2f757fca046b2b29ac849e30c25e044a
svitlarp <svitlanka.ripa@gmail.com>
2024-10-23 10:40:13 +0200

    main
    wip task-3

--- task_3.py
+++ task_3.py
@@ -1,54 +1,61 @@
 import sys
 from pathlib import Path
-# import colorama
-# from colorama import Fore, Back, Style
+import colorama
+from colorama import Fore, Back, Style
 
-# colorama.init(autoreset=True)
-# print(Fore.MAGENTA + str(var))
+
+def displaying_dir_content(path):
+    '''
+#     a script that takes a directory path as a command line argument 
+#     and visualizes the structure of that directory, 
+#     displaying the names of all subdirectories and files.
+#     '''
+    handle_path(Path(path)) 
+    # # print(f'{given_path}/')
+    # # Check if path is a string
+    # if isinstance(path, str):
+    #     path_obj = Path(path)
+    #     # Check if given path exists and if is a dir
+    #     if path_obj.exists():
+    #         print(f'{path_obj}/ ')
+    #         # if is a correct path display all directory content
+    #         handle_path(path_obj)   
+    #     else:
+    #         return 'no file'
+    # else:
+    #     return 'not correct type'    
+
+
+
+def handle_path(path_obj: Path):    
+
+    # Iterate through all files in the folder (and subfolders)
+
+    for file in path_obj.iterdir():
+        if file.is_dir():
+            relative_path = file.relative_to(path_obj)
+            print(f'\n\t {relative_path}/')
+            # print(Back.LIGHTGREEN_EX + f'\n\t {relative_path}/')
+            displaying_dir_content(file)
+        else:
+            relative_path = file.relative_to(path_obj)
+            # print(f'- {relative_path}') 
+            print(Fore.MAGENTA + f'- {relative_path}')
+            
+
+
 
 
 # print(file_name)
 # given_path = ('bdfb') 
 # given_path = ('..') 
+# given_path = (789) 
 given_path = '../modul5_files'
+# given_path = 'modul5_files'
+# given_path = 'intro.py' # if path is a file
 
-
-def displaying_dir_content(directory):
-    print(str(directory))
-
-    path_obj = Path(directory)
-
-    for file in path_obj.iterdir():
-        # print(file)
-        if file.is_dir():
-            # relative_path = file.relative_to(directory)
-            # print(f'    {relative_path}/')
-            displaying_dir_content(file)
-        else:
-            relative_path = file.relative_to(directory)
-            print(f'- {relative_path}')
 
 print(displaying_dir_content(given_path))            
 
-
-
-
-# def displaying_dir_content(path):
-#     '''
-#     a script that takes a directory path as a command line argument 
-#     and visualizes the structure of that directory, 
-#     displaying the names of all subdirectories and files.
-#     '''
-#     # Check if path is a string
-#     if isinstance(path, str):
-#     # TODO check якщо вказаний шлях не існує
-
-#         path_obj = Path(path)
-
-#         # Iterate through all files in the folder (and subfolders)
-  
-
-
-# print(displaying_dir_content(given_path))
 
 
